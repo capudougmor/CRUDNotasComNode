2e0772963917f3ab076a0cc562337e39ad38217f
Douglas Mendes Assunção <capudougmor@gmail.com>
2020-05-25 20:43:56 +0000

    Criacao dos modelos do banco de dado

---
+++ src/models/User.js
@@ -0,0 +1,31 @@
+const {Schema, model} = require('mongoose')
+const bcriptjs = require('bcryptjs')
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+})
+
+UserSchema.methods.encrypPassword = async password => {
+    const salt = await bcriptjs.genSalt(10)
+    return await bcriptjs.hash(password, salt)
+}
+
+UserSchema.methods.matchPassword = async password => {
+    return await bcriptjs.compare(password, this.password)
+
+}
+
+module.exports = model('User', UserSchema)
