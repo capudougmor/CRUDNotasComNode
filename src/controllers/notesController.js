13a21ec704c2eceec7e774c3cbb21ed46248efaf
Douglas Mendes Assunção <capudougmor@gmail.com>
2020-05-27 14:45:55 +0000

    Definido as rotas notes

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -8,5 +8,21 @@ notesController.createNewNote = (req, res) => {
     res.send('Rota criada')
 }
 
+notesController.renderNotes = (req, res) => {
+    res.send('Todas as notas')
+}
+
+notesController.renderEditForm = (req, res) => {
+    res.send('Form edit')
+}
+
+notesController.updateNotes = (req, res) => {
+    res.send('Update notes')
+}
+
+notesController.deleteNote = (req, res) => {
+    res.send('deleted note')
+}
+
 
 module.exports = notesController
--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -1,9 +1,16 @@
 const { Router } = require('express')
 const router = Router()
 
-const { renderNoteForm, createNewNote } = require('../controllers/notesController')
+const { renderNoteForm, createNewNote, renderNotes, renderEditForm, updateNotes, deleteNote } = require('../controllers/notesController')
 
 router.get('/note/add', renderNoteForm)
 router.post('/note/add', createNewNote)
+
+router.get('/notes', renderNotes)
+router.get('/notes/edit/:id', renderEditForm)
+router.put('/notes/edit/:id', updateNotes)
+router.delete('/notes/delete/:id', deleteNote)
+
+
 
 module.exports = router
